f7dd4134b24a102a71a35dfef2b00e0519451733
ElJakes <jidejacobs1@gmail.com>
2023-07-24 17:47:30 +0100

    add first code to project

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#input-el')
+const addBtn = document.querySelector('#add-btn')
+
+
+addBtn.addEventListener('click', () => {
+    const inputValue = inputEl.value
+    console.log(inputValue)
+})
